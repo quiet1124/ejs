4ae5db00fb8ce3f7f3ecef75d976ac861d99a2dc
choiyechan <homestayout@gmail.com>
2020-05-21 17:13:01 +0900

    app set

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express')
-
+var path = require('path')
+var bodyParser = require('body-parser')
 var app = express()
+
+
+app.set('views', path.join(__dirname+'/views'))
+app.set('view engine', 'ejs')
+
+
+// app.use()
 
 app.get('/', function(request, response){
     console.log(request)
-    response.send("<h1 style='color:red'>안녕하세요</h1>")
+    response.render('index')
 })
 // listen(포트 , url , backlog , callback)
 app.listen(8080, function(){
